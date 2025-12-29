80c832f6420a1d688708613cc60148e8bfebd37f
Cofallen <1727334304@qq.com>
2025-12-29 23:43:35 +0900

    first commit

---
+++ motorCheck/Horizon_Infantry/User/Start/Inc/Motors.h
@@ -0,0 +1,130 @@
+#ifndef __MOTORS_H
+#define __MOTORS_H
+
+#include "DJI_Motor.h"
+#include "DM_Motor.h"
+
+typedef struct
+{
+    DJI_MOTOR_Typedef DJI_6020_Pitch;
+    DJI_MOTOR_Typedef DJI_6020_Yaw;
+
+    DJI_MOTOR_Typedef DJI_3508_Shoot_L;
+    DJI_MOTOR_Typedef DJI_3508_Shoot_R;
+    DJI_MOTOR_Typedef DJI_3508_Shoot_M;
+
+    DJI_MOTOR_Typedef DJI_3508_Chassis_1;
+    DJI_MOTOR_Typedef DJI_3508_Chassis_2;
+    DJI_MOTOR_Typedef DJI_3508_Chassis_3;
+    DJI_MOTOR_Typedef DJI_3508_Chassis_4;
+
+    DM_MOTOR_Typedef left_front;
+    DM_MOTOR_Typedef left_back;
+    DM_MOTOR_Typedef right_front;
+    DM_MOTOR_Typedef right_back;
+
+    DJI_MOTOR_Typedef left_wheel;
+    DJI_MOTOR_Typedef right_wheel;
+
+}MOTOR_Typedef;
+
+typedef struct
+{
+    /******************************遥控********************************/
+    uint8_t RM_DBUS;
+    /******************************运行模式********************************/
+    uint8_t RM_MOD;
+    /******************************头部电机********************************/
+    uint8_t MOTOR_HEAD_Pitch;
+    uint8_t MOTOR_HEAD_Yaw;
+    /******************************发射电机********************************/
+    uint8_t MOTOR_Shoot_L;
+    uint8_t MOTOR_Shoot_R;
+    uint8_t MOTOR_Shoot_M;
+    /******************************底盘电机********************************/
+    uint8_t MOTOR_Chassis_1;
+    uint8_t MOTOR_Chassis_2;
+    uint8_t MOTOR_Chassis_3;
+    uint8_t MOTOR_Chassis_4;
+    /******************************底盘电容********************************/
+    uint8_t Power;
+    /******************************主控位置********************************/
+    uint8_t MASTER_LOCATION;
+
+}RUI_ROOT_STATUS_Typedef;
+
+typedef struct
+{
+    struct
+    {
+        float VX;
+        float VY;
+        float VW;
+        float wheel1;
+        float wheel2;
+        float wheel3;
+        float wheel4;
+        uint8_t CAP;
+
+    } BOTTOM;
+
+    struct
+    {
+        float Pitch;
+        float Pitch_MAX;//常量
+        float Pitch_MIN;//常量
+        float Yaw;
+
+    } HEAD;
+
+    struct
+    {
+        float SHOOT_L;
+        float SHOOT_R;
+        float SHOOT_M;
+        float Shoot_Speed;//常量
+        int64_t Single_Angle;//常量
+    } SHOOT;
+
+    struct
+    {
+        int16_t YAW_INIT_ANGLE;//常量
+        int16_t YAW_ANGLE;
+        int16_t RELATIVE_ANGLE;
+        int16_t YAW_SPEED;
+        float TOP_ANGLE;
+    } CG;
+
+    struct
+    {
+        float Speed_err_L;
+        float Speed_err_R;
+        int32_t Angle;
+        float Speed_Aim_L;
+        float Speed_Aim_R;
+        uint8_t JAM_Flag;
+        uint32_t Shoot_Number;
+        uint32_t Shoot_Number_Last;
+    }SHOOT_Bask;
+
+    struct 
+    {
+        float RobotUI_Dtime;
+        float Move_Dtime;
+        float Defiant_Dtime;
+        float IMU_Dtime;
+        float StartRoot_Dtime;
+        float TIM7_Dtime;
+        uint32_t RobotUI_DWT_Count;
+        uint32_t Move_DWT_Count;
+        uint32_t Defiant_DWT_Count;
+        uint32_t IMU_DWT_Count;
+        uint32_t Root_DWT_Count;
+        uint32_t TIM7_DWT_Count;
+    }DWT_TIME;
+    
+    uint8_t MOD[2];
+    uint8_t ORE;
+}CONTAL_Typedef;
+
+#endif
